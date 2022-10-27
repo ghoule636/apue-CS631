299d4bd887f9e04fe9da44b7f3c1b75f96bc6862
ghoule636 <ghoule636@gmail.com>
2022-10-27 15:37:27 +0000

    moving writing, reading and opening files to separate functions. Fixed file size bug.

--- bbcp/bbcp.c
+++ bbcp/bbcp.c
@@ -10,6 +10,45 @@
 #define BUFF_SIZE 32768
 #endif
 
+int openFileForReading(char* path) {
+	int fd;
+	errno = 0;
+	if ((fd = open(path, O_RDONLY)) == -1) {
+		fprintf(stderr, "Unable to read %s: %s\n", path, strerror(errno));
+		exit(EXIT_FAILURE);
+	}	
+	return fd;
+}
+
+int openFileForWriting(char* path) {
+	int fd;
+	int file_permission_bits = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+
+	errno = 0;
+	if ((fd = open(path, O_WRONLY | O_CREAT, file_permission_bits)) == -1) {
+		fprintf(stderr, "Unable to create %s: %s\n", path, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	return fd;
+}
+
+void copyData(int fd1, int fd2) {
+	char buf[BUFF_SIZE];	
+	int read_val;
+	bzero(buf, BUFF_SIZE);
+	while ((read_val = read(fd1, buf, BUFF_SIZE)) > 0) {
+		if (write(fd2, buf, read_val) == -1) {
+			fprintf(stderr, "Unable to write: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (read_val < 0) {
+		fprintf(stderr, "Unable to read: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
 /**
  * This function will accept two params. A file and a file or directory.
  * Then it will attempt to open both files and copy from the first
@@ -18,36 +57,19 @@
  */
 int main(int argc, char **argv) {
 
-	int file1;
-	int file2;
-	int read_val;
-	char buf[BUFF_SIZE];
-	int file_permission_bits = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-	bzero(buf, BUFF_SIZE);	
+	int file1, file2;
 	
 	if (argc != 3) {
 		fprintf(stderr, "usage: %s file_name destination\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
+
+	file1 = openFileForReading(argv[1]);	
+	file2 = openFileForWriting(argv[2]);
+
+	copyData(file1, file2);
 	
-	errno = 0;	
-	if ((file1 = open(argv[1], O_RDONLY)) == -1) {
-		fprintf(stderr, "Unable to read %s: %s\n", argv[1], strerror(errno));
-		exit(EXIT_FAILURE);
-	} else if ((file2 = open(argv[2], O_WRONLY | O_CREAT, file_permission_bits)) == -1) {
-		fprintf(stderr, "Unable to create %s: %s\n", argv[2], strerror(errno));
-		exit(EXIT_FAILURE);
-	} else {
-		while ((read_val = read(file1, buf, BUFF_SIZE)) > 0) {
-			if (write(file2, buf, BUFF_SIZE) == -1) {
-				fprintf(stderr, "Unable to write to %s: %s\n", argv[2], strerror(errno));
-				exit(EXIT_FAILURE);
-			 }
-		}
-		if (read_val < 0) {
-			fprintf(stderr, "Unable to read from %s: %s\n", argv[1], strerror(errno));
-			exit(EXIT_FAILURE);
-		}
-	}
+	close(file1);
+	close(file2);
 	return(EXIT_SUCCESS);
  }
